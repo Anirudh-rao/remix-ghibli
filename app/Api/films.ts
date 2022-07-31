6c643e4d7d0d753a70df836046469de9b021808a
Anirudh-rao <anirudh131299@gmail.com>
2022-07-31 21:04:39 +0530

    Added Some Components

--- app/Api/films.ts
+++ app/Api/films.ts
@@ -1,45 +1,62 @@
-//Films List
-export type Film = {
-    id:string,
-    title:string,
-    original_title:string,
-    description:string,
-    image:string,
-    movie_banner:string;
-    people:string[];
-    characters?: FilmCharacter[];
-};
-//Film Charecters list
+import { CommentEntry, getComments } from "./comments";
+
 export type FilmCharacter = {
-    id: string;
-    name: string;
-    gender?: string;
-    age?: string;
-    eye_color?: string;
-    hair_color?: string;
+  id: string;
+  name: string;
+  gender?: string;
+  age?: string;
+  eye_color?: string;
+  hair_color?: string;
 };
 
-//Async function to get Films
-export async function GetFilms(title?: string | null) {
-    const response =  await fetch('https://ghibliapi.herokuapp.com/films');
-    const films:Film[] = await response.json();
-    return films.filter((film)=>
-    //Matching our Title 
-    title? film.title.toLowerCase().includes(title.toLowerCase()):true
-    );
+export type Film = {
+  id: string;
+  title: string;
+  original_title: string;
+  description: string;
+  image: string;
+  movie_banner: string;
+  people: string[];
+  characters?: FilmCharacter[];
+  comments?: CommentEntry[]
+};
+
+export async function getFilms(title?: string | null) {
+  const response = await fetch('https://ghibliapi.herokuapp.com/films');
+
+  const films: Film[] = await response.json();
+
+  return films.filter((film) =>
+    title ? film.title.toLowerCase().includes(title.toLowerCase()) : true
+  );
 }
 
-//Async Function to Get Film ID 
-export async function GetFilmID(filmId: string ) {
-    const response =  await fetch(`https://ghibliapi.herokuapp.com/films/${filmId}`);
-    const film:Film = await response.json(); 
-    //returning A Film Array
+export async function getFilmById(filmId: string) {
+  const response = await fetch(
+    `https://ghibliapi.herokuapp.com/films/${filmId}`
+  );
 
-    //Getting A Charecter Array
-    const characters = await Promise.all(
-        film.people
-          .filter((url: string) => url !== 'https://ghibliapi.herokuapp.com/people/')
-          .map((url: URL | RequestInfo) => fetch(url).then((res) => res.json()))
-      );
-    return {film, characters};
+  const film: Film = await response.json();
+
+  const comments = await getComments(filmId);
+
+  const characters = await Promise.all(
+    film.people
+      .filter((url) => url !== 'https://ghibliapi.herokuapp.com/people/')
+      .map((url) => fetch(url).then((res) => res.json()))
+  );
+
+  return { ...film, characters, comments };
+}
+
+export async function getFilmCharacter(characterId: string): Promise<FilmCharacter> {
+  const response = await fetch(
+    `https://ghibliapi.herokuapp.com/people/${characterId}`
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  return response.json();
 }
